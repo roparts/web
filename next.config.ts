ef8b8e485b99977d11b76f817ba83ff51cf43a1c
Ro parts <droproparts@gmail.com>
2025-07-15 23:10:24 +0000

    can you add images

--- next.config.ts
+++ next.config.ts
@@ -16,8 +16,16 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
 
 export default nextConfig;
+
+    
